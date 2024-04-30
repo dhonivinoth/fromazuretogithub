ab212f250edc875ecae6827b3a219abaa8ac57d7
professionalazure <professionalazuremail@gmail.com>
2024-04-30 07:16:16 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     storage_account_name = "firststg702"
-    container_name       = "deveopscontainer"
+    container_name       = "devopscontainer"
     key                  = "vm terraform"
     resource_group_name  = "firstrg701"
   }
