68040b324290da4a63249879ae039416a6d6ce2b
professionalazure <professionalazuremail@gmail.com>
2024-05-09 05:40:17 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "vn01" {
 resource "azurerm_subnet" "sn01" {
   name                 = "vino sn"
   resource_group_name  =data.azurerm_resource_group.rg.name
-  virtual_network_name =data.azurerm_virtual_network.vn01.name
+  virtual_network_name =azurerm_virtual_network.vn01.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
