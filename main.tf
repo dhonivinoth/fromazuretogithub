8a9d8f3a29722482087033538d7a43b787578ada
professionalazure <professionalazuremail@gmail.com>
2024-05-13 12:40:58 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vn01" {
-  name                = "vino vn"
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            =data.azurerm_resource_group.rg.location
   resource_group_name =data.azurerm_resource_group.rg.name
